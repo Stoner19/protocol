201516dcba34b5a17932caa5eb11341c1d10d386
Yang Li <stephen.liy@gmail.com>
2018-08-22 14:58:14 +0800

    add the code for test_rpc

--- ovm/test_rpc/server.js
+++ ovm/test_rpc/server.js
@@ -2,6 +2,8 @@ const logger = require('./logger').logger;
 const appFactory = require('./providers/appProvider').factory;
 //const web3Factory = require('./providers/web3Provider').factory;
 
+const  oneledgerNetworkService = require('./services/OneledgerNetworkService').instance();
+
 exports.run = (env)=>{
   const config = require(`./configs/${env}`).config;
   const [app, port] = appFactory(config);
@@ -9,6 +11,8 @@ exports.run = (env)=>{
   const context = {
     app, port
   }
+
+  oneledgerNetworkService.init(context);
   //register the service
 
   //end of register
