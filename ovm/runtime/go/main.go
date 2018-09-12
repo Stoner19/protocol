7ee7144147773b53e21f19233a331ec5ecf27ba6
Yang Li <stephen.liy@gmail.com>
2018-09-12 10:57:16 -0400

    get the code from blockchain

--- ovm/runtime/go/main.go
+++ ovm/runtime/go/main.go
@@ -5,13 +5,16 @@ import (
   "github.com/robertkrimen/otto"
 )
 
+// this is going to get the code from block chain by the address string
+func getCodeFromBlockChain(address string) string{
+  return `console.log('hello, world')`
+}
+
 func main() {
 	fmt.Println("starting OVM")
   vm := otto.New()
-  vm.Set("version", "OVM 0.1 simple test");
-  vm.Run(`
-    abc = 2+2;
-    console.log("the value of abc is " + abc);
-    console.log("running on the vm " + version);
-    `)
+  vm.Set("version", "OVM 0.1 TEST")
+  vm.Set("context", `{foo: 'bar'}`)
+  vm.Run(getCodeFromBlockChain("0x0000000000"))
+  fmt.Println("ending OVM")
 }
