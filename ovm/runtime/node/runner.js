eff0acb5160afe423233857e97f626fedfa63543
Yang Li <stephen.liy@gmail.com>
2018-09-24 10:38:59 -0400

    add Set

--- ovm/runtime/node/runner.js
+++ ovm/runtime/node/runner.js
@@ -4,5 +4,7 @@ var Contract = require('./simple_contract');
 var contract = new Contract(context);
 
 contract.setWord('myaddress','hello,world')
-var output = contract.getWord('myaddress')
+var output = contract.getWord('myaddress');
+
+
 console.log(output);
