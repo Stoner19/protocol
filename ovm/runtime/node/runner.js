418f08734167fbff18cddd11319546511abdedf9
Yang Li <stephen.liy@gmail.com>
2018-09-25 19:46:32 -0400

    add first implementation of go

--- ovm/runtime/node/runner.js
+++ ovm/runtime/node/runner.js
@@ -4,7 +4,16 @@ var Contract = require('./simple_contract');
 var contract = new Contract(context);
 
 contract.setWord('myaddress','hello,world')
-var output = contract.getWord('myaddress');
+var ret = contract.getWord('myaddress');
 
 
-console.log(output);
+console.log(ret);
+var list = context.getUpdateIndexList();
+var storage = context.getStorage();
+var transaction = {};
+for (var i = 0; i< list.length; i ++) {
+  var key = list[i];
+  transaction[key] = storage[key];
+}
+
+console.log(JSON.stringify(transaction));
