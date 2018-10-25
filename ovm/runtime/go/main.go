69d114e7d2f1b57a674e39d5957b6c2347539f0b
Yang Li <stephenli@oneledger.io>
2018-10-25 14:26:56 -0400

    add monitor

--- ovm/runtime/go/main.go
+++ ovm/runtime/go/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "time"
   "fmt"
-  "./runner"
   "os"
+  "./runner"
+  "./monitor"
 )
 
 func run(x chan string, y chan string) {
@@ -14,39 +14,33 @@ func run(x chan string, y chan string) {
   y <- returnValue
 }
 
-func monitor(status_ch chan string) {
-  i := 0
-  threshold := 60
-  for {
-    time.Sleep(time.Second)
-    i = i + 1
-    if i >= threshold {
-      fmt.Println("something is wrong")
-      status_ch <- "crash the runtime"
-      return
-    }
-  }
-}
-
 func main() {
   fmt.Println("starting OVM")
-  defer func() {
-    if r := recover(); r != nil {
-      panic(r);
-    }
-    os.Remove("./ovm.pid")
-  }()
-
-  if _, err := os.Stat("./ovm.pid"); !os.IsNotExist(err) {
-    panic("ovm.pid file exists, there is another ovm running or exit abnormally")
-  }
-
-  os.Create("./ovm.pid")
-
   transaction_ch := make(chan string)
   returnValue_ch := make(chan string)
-  status_ch := make(chan string)
-  go monitor(status_ch)
+  status_ch := make(chan monitor.Status)
+  monitor := monitor.CreateMonitor(10, monitor.DEFAULT_MODE, "./ovm.pid")
+
+  status, err := monitor.CheckUnique()
+
+  defer func() {
+    if r := recover(); r != nil {
+      fmt.Println(status)
+      panic(r);
+    }
+    os.Remove(monitor.GetPidFilePath())
+  }()
+
+  if err == true  {
+    panic(status)
+  } else {
+    fmt.Println(status)
+  }
+
+  os.Create(monitor.GetPidFilePath())
+
+
+  go monitor.CheckStatus(status_ch)
   go run(transaction_ch, returnValue_ch)
   ready := 0
   for {
@@ -58,7 +52,7 @@ func main() {
       fmt.Println(returnValue)
       ready ++
     case status := <- status_ch:
-      fmt.Println("retuning: ", status)
+      fmt.Println("retuning: ", status.Details, "with code", status.Code)
       panic("exit with code -1")
     }
     if ready == 2 {
