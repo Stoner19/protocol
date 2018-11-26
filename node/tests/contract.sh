a766d0a94ed8971269be91e8a005dac4f8579235
pat-oneledger <pat@oneledger.io>
2018-11-26 08:34:45 -0400

    test script for smart contract install and execute

--- node/tests/contract.sh
+++ node/tests/contract.sh
@@ -3,10 +3,16 @@
 #
 # Test creating a single send transaction in a 1-node chain, reset each time
 #
-CMD=$GOPATH/src/github.com/Oneledger/protocol/node/scripts
+CMD=$GOPATH/src/github.com/Oneledger/protocol/node
 
 
 echo "=================== Send Transactions =================="
-olclient install -c David --owner David-OneLedger --name Test --version v0.0.1 -f test.old
-olclient execute -c David --owner David-OneLedger --name TestExecute --version v0.10.1
+olclient install -c David --owner David-OneLedger --name Test --version v0.0.1 -f $CMD/tests/contract1.txt
+olclient install -c David --owner David-OneLedger --name Test --version v0.0.2 -f $CMD/tests/contract2.txt
+olclient install -c David --owner David-OneLedger --name Test2 --version v0.0.1 -f $CMD/tests/contract3.txt
+
+olclient execute -c David --owner David-OneLedger --name Test --version v0.0.2
+olclient execute -c David --owner David-OneLedger --name Test2 --version v0.0.1
+
+$CMD/scripts/stopDevNet
 
